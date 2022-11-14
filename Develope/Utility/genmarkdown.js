4f0d9be5d749bc74711959a7c0e4a4fc3cd6e699
Dominic <dompaulazzo@gmail.com>
2022-11-14 12:58:33 -0800

    added some of the topics for the table of contents and section inputs

--- Develope/Utility/genmarkdown.js
+++ Develope/Utility/genmarkdown.js
@@ -1,49 +1,79 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+const fs = require('fs');
 
-// TODO: Create a function that returns the license link
+// Returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseBadge(license) {
+  if (!license) {
+    return ``;
+  } else {
+    return `[![${license} license](https://img.shields.io/badge/License-${license}-blue.svg)](${renderLicenseLink(license)})`
+  }
+}
 
-// TODO: Create a function that returns the license section of README
+// Returns the license link
+function renderLicenseLink(license) {
+  if (license === 'MIT') {
+    return `https://lbesson.mit-license.org/`
+  }
+  if (license === 'GPL') {
+    return `http://perso.crans.org/besson/LICENSE.html`
+  }
+  if (license === 'CC--0') {
+    return `https://creativecommons.org/licenses/by-nd/4.0` 
+  }
+}
+
+// Returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (!license) {
+    return ``;
+  } else {
+    return `## Licenses
+    This project is covered under the ${license} license. To learn more about what this means, click the license button at the top.`
+  }
+}
 
-// TODO: Create a function to generate markdown for README
+// Generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
-  ## Table of Contents 
-  - [Project Description](#Description)
-  - [Installation](#Installation)
-  - [License](#License)
-  - [Contributing](#Contributing)
-  - [Usage Description](#Usage)
-  - [Tests](#Tests)
-  - [Questions](#Questions)
+  ${renderLicenseBadge(data.licenses)}
 
-  ##Description
-  ${data.description} 
+  ## Table of Contents
+  * [Description](#description)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Licenses](#licenses)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  * [Credits](#credits)
 
-  ##Installation
+  ## Description
+  ${data.description}
+
+  ## Installation
   ${data.installation}
 
-  ##License
-  ${data.license}
-
-  ##Contributing
-  ${data.contributing}
-
-  ##Usage
+  ## Usage
   ${data.usage}
 
-  ##Tests
+  ${renderLicenseSection(data.licenses)}
+
+  ## Contributing
+  ${data.contributing}
+
+  ## Tests
   ${data.tests}
 
-  ##Questions
-  ${data.questions}
+  ## Questions
+  Any and all questions welcomed! Please list below!  
+  GitHub: https://github.com/${data.github}  
+  Email: ${data.email}
 
+  ## Credits
+  ${data.name}
 `;
 }
 
